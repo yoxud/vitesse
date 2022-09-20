4c7013344992d24879e7e3596754dc19f721c6cc
Yorgo Xudous <yorgo@le1640.com>
2022-09-20 00:42:47 -0400

    test

--- src/store/products.ts
+++ src/store/products.ts
@@ -4,7 +4,7 @@ import type { Product } from '../types'
 export const useProductStore = defineStore('products', () => {
   const products = ref(new Array<Product>())
   const ready = ref(false)
-
+  // test
   function init() {
     products.value.push({ id: 1, name: 'banana', cost: 200 })
     products.value.push({ id: 2, name: 'apple', cost: 300 })
